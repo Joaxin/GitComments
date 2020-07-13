2dd814a23d7267f68d3e42d90180c08d0d43ab45
Joaxin <lotility@gmail.com>
2020-07-13 22:43:17 +0800

    Rxd.py support .rmd python

--- RmdtoHexo/Rxd.py
+++ RmdtoHexo/Rxd.py
@@ -3,15 +3,15 @@ import re
 import os
 path = os.getcwd()
 flist = os.listdir(path)
-
+os.makedirs('convert', exist_ok=True)
 for fl in flist:
     fhtml = re.search(r'.*?(\.html)$',fl)
     if fhtml:
         print("正在处理 " + fhtml.group())
         with open(path + '/' + fl,'rb') as fp:
             soup_pre = fp.read().decode('utf-8')
-            formatter = re.search('(---.*?---)',soup_pre, re.S).group(1)
-            if formatter:
+            if re.search('(---.*?---)',soup_pre, re.S):
+                formatter = re.search('(---.*?---)',soup_pre, re.S).group(1)
                 soup_pre = re.sub('(---.*?---)','',soup_pre,flags = re.S)
             
             soup = BeautifulSoup(soup_pre,"lxml")
@@ -26,9 +26,15 @@ for fl in flist:
 
             code = soup.select("div.sourceCode")
             print("截获到" + str(len(code)) + "个代码片段\n")
-            wrapper1 = '<figure class="highlight R"><table><tbody><tr><td class="code">'
             wrapper2 = '</tr></tbody></table></figure>'
             for cd in soup.select("div.sourceCode"):
+                if re.search('sourceCode python', str(cd)):
+                    lang = 'python'
+                elif re.search("sourceCode r", str(cd)):
+                    lang = 'R'
+                else:
+                    lang=""
+                wrapper1 = '<figure class="highlight ' + lang + '"><table><tbody><tr><td class="code">'
                 cc = cd.code
                 cc.name = "pre"
                 ccc = wrapper1 + str(cc) + wrapper2
@@ -55,7 +61,7 @@ for fl in flist:
             }
             for pat in pattern:
                 soup = re.sub(pat,pattern[pat],str(soup))
-        with open(path + '/' +  fl,'wb') as fp:
+        with open(path + '\\convert\\' +  fl,'wb') as fp:
             soup_after = formatter + '\n' + str(soup)
             fp.write(soup_after.encode('utf-8'))
 
